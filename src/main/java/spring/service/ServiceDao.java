0cda7987db51849f0b7268d4ccad5ba865441521
LShere <384757457@qq.com>
2019-08-24 22:24:41 +0800

    openid加入数据库，方便后期存取使用

--- src/main/java/spring/service/ServiceDao.java
+++ src/main/java/spring/service/ServiceDao.java
@@ -5,7 +5,7 @@ import spring.pojo.User;
 import java.util.List;
 
 public interface ServiceDao {
-    public List<User> findUser();
-    public List<User> findUserById(long id);
-    public User findUserByName(String name);
+//    public List<User> findUser();
+//    public List<User> findUserById(long id);
+//    public User findUserByName(String name);
 }
---
+++ src/main/java/spring/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.dao.UserDao;
+import spring.pojo.User;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserDao userDao;
+
+    public Integer addUser(User user) {
+        return userDao.addUser(user);
+    }
+
+    public User findUserById(String openid) {
+        return userDao.findUserById(openid);
+    }
+
+    public User findUserByName(String nickname) {
+        return userDao.findUserByName(nickname);
+    }
+}
