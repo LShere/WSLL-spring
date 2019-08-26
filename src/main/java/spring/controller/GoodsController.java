06ec891edd15b6f7a822e736fa5ed6feb34fd9ba
oxHang <354058255@qq.com>
2019-08-26 10:07:45 +0800

    添加分页查询和根据id查询

--- src/main/java/spring/controller/GoodsController.java
+++ src/main/java/spring/controller/GoodsController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import spring.pojo.Goods;
+import spring.pojo.Page;
 import spring.service.GoodsService;
 
 import java.util.HashMap;
@@ -34,4 +35,39 @@ public class GoodsController {
         }
 
     }
+
+    @GetMapping(value = "/selectGoodsPage")
+    @ResponseBody
+    public Map<String, Object> findGoodPage(int currentPage, int goods_id, String goods_type, String goods_name, String goods_describe){
+        Map<String, Object> map = new HashMap<String, Object>();
+        Page<Goods> goodsPage=goodsService.findGoodPage(currentPage,goods_id,goods_type,goods_name,goods_describe);
+        if (goodsPage == null) {
+            map.put("code", 400);
+            map.put("message", "不存在商品!");
+            return map;
+        } else {
+            map.put("code", 200);
+            map.put("message", "查找商品成功!");
+            map.put("goods", goodsPage);
+            return map;
+        }
+    }
+
+    @GetMapping(value = "/findGoodsbyId")
+    @ResponseBody
+    public Map<String, Object> findGoodsbyId(int id){
+        Map<String, Object> map = new HashMap<String, Object>();
+        Goods goods=goodsService.findGoodsById(id);
+        if (goods == null) {
+            map.put("code", 400);
+            map.put("message", "不存在商品!");
+            return map;
+        } else {
+            map.put("code", 200);
+            map.put("message", "查找商品成功!");
+            map.put("goods", goods);
+            return map;
+        }
+    }
+
 }
--- src/main/java/spring/service/GoodsServiceImpl.java
+++ src/main/java/spring/service/GoodsServiceImpl.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.dao.GoodsDao;
 import spring.pojo.Goods;
+import spring.pojo.Page;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -15,5 +17,44 @@ public class GoodsServiceImpl implements GoodsService {
 
     public List<Goods> findAllGoods() {
         return this.goodsDao.findAllGoods();
+    }
+
+    /*实现分页查询*/
+    public Page<Goods> findGoodPage(int currentPage, int goods_id, String goods_type, String goods_name, String goods_describe) {
+        HashMap<String,Object> map=new HashMap<String, Object>();
+        Page<Goods> page=new Page<Goods>();
+
+        /*写入总记录数
+        * 写入当前页数
+        * 获取每页记录行数*/
+        page.setTotalUsers(goodsDao.countGoods());
+        page.setCurrentPage(currentPage);
+        int pageSize=page.getPageSize();
+
+        /*将当前页数和每页记录行数写入map*/
+        map.put("currentPage",(currentPage-1)*pageSize);
+        map.put("pageSize",pageSize);
+
+        if (goods_id >= 0) {
+            map.put("goods_id",goods_id);
+        }
+        if (goods_type != null) {
+            map.put("goods_type",goods_type);
+        }
+        if (goods_name != null) {
+            map.put("goods_name",goods_name);
+        }
+        if (goods_describe != null) {
+            map.put("goods_describe",goods_describe);
+        }
+
+        List<Goods> list=goodsDao.findGoodsPage(map);
+        page.setList(list);
+
+        return page;
+    }
+
+    public Goods findGoodsById(int id) {
+        return this.goodsDao.findGoodsById(id);
     }
 }
