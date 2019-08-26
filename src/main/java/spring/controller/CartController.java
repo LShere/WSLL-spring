96dc1ed639d3943b464818c07d1c136c8462877f
WK <940515984@qq.com>
2019-08-26 16:02:39 +0800

    购物车计数add

---
+++ src/main/java/spring/controller/CartController.java
@@ -0,0 +1,19 @@
+package spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import spring.service.CartService;
+
+@Controller
+public class CartController {
+    @Autowired
+    CartService cartService;
+    @RequestMapping(value = "/countCart")
+    @ResponseBody
+    public int countCart(String openid){
+        return  cartService.countCart(openid);
+    }
+}
